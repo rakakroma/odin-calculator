5fb389e871df1aa880b546a00583b954face17e3
rakakroma <102795114+rakakroma@users.noreply.github.com>
2022-05-06 19:31:58 +0800

    but no css

--- app.js
+++ app.js
@@ -11,6 +11,9 @@ function operate(arr) {
         arr[2] = arr.slice(2).join("");
         arr.length = 3;
     }
+    if (typeof operationArray[operationArray.length - 1] === 'object') {
+        return +arr[0];
+    }
     if (arr[1][0] === '+') {
         return +arr[0] + +arr[2]
     } else if (arr[1][0] === '-') {
@@ -34,7 +37,7 @@ numbers.forEach(function (num) {
     num.addEventListener('click', function () {
         operationArray.push(this.id);
         console.log(operationArray);
-        displayBoard.innerHTML = operationArray.join("");
+        displayBoard.textContent = operationArray.join("");
     })
 }
 )
@@ -46,19 +49,19 @@ operators.forEach(function (operator) {
         if (operationArray.length === 0) {
             operationArray[0] = solution;
         } else if (typeof operationArray[1] === 'object') {
-            solution = operate(operationArray).toFixed(5);
+            solution = operate(operationArray);
             solutionBoard.textContent = solution;
             operationArray.length = 0;
             operationArray[0] = solution;
             operationArray.push([this.id]);
-            displayBoard.innerHTML = operationArray.join("");
+            displayBoard.textContent = operationArray.join("");
             return;
         }
         console.log(operationArray)
         operationArray[0] = operationArray.join("");
         operationArray.length = 1;
         operationArray.push([this.id]);
-        displayBoard.innerHTML = operationArray.join("");
+        displayBoard.textContent = operationArray.join("");
     })
 }
 )
@@ -70,7 +73,7 @@ operators.forEach(function (operator) {
 
 const operationKey = document.querySelector('#equal')
 operationKey.addEventListener('click', function () {
-    solution = operate(operationArray).toFixed(5);
+    solution = operate(operationArray);
     solutionBoard.textContent = solution;
     operationArray.length = 0;
 })
@@ -93,3 +96,11 @@ clear.addEventListener('click', function () {
     displayBoard.innerHTML = '0';
 })
 //clear just clear the operationArray, but doesn't clear solution
+
+const backspace = document.querySelector('#backspace')
+backspace.addEventListener('click', function () {
+    if (operationArray.length > 0) {
+        operationArray.pop();
+        displayBoard.textContent = operationArray.join("");
+    }
+})
